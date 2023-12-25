63b052a933d950f37bcb2f6f8aab5192721401ef
saeed.da1994 <saeed.da1994@gmail.com>
2023-12-25 09:23:16 +0330

    -grab all dynamic element in weather box after fetch data

--- scripts/app.js
+++ scripts/app.js
@@ -3,4 +3,24 @@ const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 
+search.addEventListener('click', () => {
+    const APIkey = '55f428ad7a8370ee4852c41ecd5c1edb';
+    const city = document.querySelector('.search-box input').value;
+
+    if (city === '')
+        return false;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIkey}`)
+        .then(response => response.json())
+        .then(json =>{
+            console.log(json)
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .info-humidity span');
+            const wind = document.querySelector('.weather-details .info-wind span');
+
+
+        })
+})
 

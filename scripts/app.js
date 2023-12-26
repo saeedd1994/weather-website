6a5acab2dd651c53a043b9cdd8a6ca48aa24c380
saeed.da1994 <saeed.da1994@gmail.com>
2023-12-26 19:02:50 +0330

    -changing image using api behavior

--- scripts/app.js
+++ scripts/app.js
@@ -12,13 +12,46 @@ search.addEventListener('click', () => {
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIkey}`)
         .then(response => response.json())
-        .then(json =>{
+        .then(json => {
             console.log(json)
             const image = document.querySelector('.weather-box img');
             const temperature = document.querySelector('.weather-box .temperature');
             const description = document.querySelector('.weather-box .description');
             const humidity = document.querySelector('.weather-details .info-humidity span');
             const wind = document.querySelector('.weather-details .info-wind span');
+
+            // changing weather image using api response
+            switch (json.weather[0].main) {
+                case 'Clear' :
+                    image.src = "images/clear.png";
+                    break;
+
+                case 'Rain' :
+                    image.src = "images/rain.png";
+                    break;
+
+                case 'Snow' :
+                    image.src = "images/snow.png";
+                    break;
+
+                case 'Clouds' :
+                    image.src = "images/cloud.png";
+                    break;
+
+                case 'Mist' :
+                    image.src = "images/mist.png";
+                    break;
+
+                case 'Haze' :
+                    image.src = "images/mist.png";
+                    break;
+
+                default:
+                    image.src = "images/cloud.png";
+                    break;
+            }
+
+
 
 
         })
